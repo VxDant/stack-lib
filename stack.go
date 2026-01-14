323643b4cde4601b26f154c417fb3ba817a9bd68
Vedant Achtani <72167208+VxDant@users.noreply.github.com>
2026-01-15 00:09:30 +0100

    fixed - go mod init v0.1.3

--- stack.go
+++ stack.go
@@ -5,19 +5,19 @@ import "sync"
 // Stack represents a LIFO data structure
 type Stack struct {
 	sync     sync.RWMutex
-	Elements []interface{}
+	elements []interface{}
 }
 
 // New creates and returns a new Stack
 func New() *Stack {
-	return &Stack{Elements: make([]interface{}, 0)}
+	return &Stack{elements: make([]interface{}, 0)}
 }
 
 // Push adds an element to the top of the stack
 func (st *Stack) Push(value interface{}) {
 	st.sync.Lock()
 	defer st.sync.Unlock()
-	st.Elements = append(st.Elements, value)
+	st.elements = append(st.elements, value)
 }
 
 // Pop removes and returns the top element
@@ -25,12 +25,12 @@ func (st *Stack) Pop() interface{} {
 	st.sync.Lock()
 	defer st.sync.Unlock()
 
-	if len(st.Elements) == 0 {
+	if len(st.elements) == 0 {
 		return nil
 	}
-	l := len(st.Elements)
-	value := st.Elements[l-1]
-	st.Elements = st.Elements[:l-1]
+	l := len(st.elements)
+	value := st.elements[l-1]
+	st.elements = st.elements[:l-1]
 	return value
 }
 
@@ -38,15 +38,15 @@ func (st *Stack) Pop() interface{} {
 func (st *Stack) Peek() interface{} {
 	st.sync.RLock()
 	defer st.sync.RUnlock()
-	if len(st.Elements) == 0 {
+	if len(st.elements) == 0 {
 		return nil
 	}
-	l := len(st.Elements)
-	value := st.Elements[l-1]
+	l := len(st.elements)
+	value := st.elements[l-1]
 	return value
 }
 
-// IsEmpty returns true if the stack has no Elements
+// IsEmpty returns true if the stack has no elements
 func (st *Stack) IsEmpty() bool {
-	return len(st.Elements) == 0
+	return len(st.elements) == 0
 }
